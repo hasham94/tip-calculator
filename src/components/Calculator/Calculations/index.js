a91d9a843863cb4179f9de0953ddc67425eff677
Hasham-Iways <hasham.ahmed@i-ways.net>
2022-02-06 15:15:44 +0500

    calculations UI

--- src/components/Calculator/Calculations/index.js
+++ src/components/Calculator/Calculations/index.js
@@ -4,10 +4,21 @@ import "./styles.css";
 const Calculations = () => {
   return (
     <div className="resultsWrapper">
-      <span>
-        SPLI <br />
-        TTER
-      </span>
+      <div className="results">
+        <div className="resultBox">
+          <h3>
+            Tip Amount <span>/ person</span>
+          </h3>
+          <span className="amount">$0.00</span>
+        </div>
+        <div className="resultBox">
+          <h3>
+            Total <span>/ person</span>
+          </h3>
+          <span className="amount">$0.00</span>
+        </div>
+      </div>
+      <button>Reset</button>
     </div>
   );
 };
