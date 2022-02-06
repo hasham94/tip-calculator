7a72cbddc2c77fc50590a9103ca69d16e4920385
Hasham-Iways <hasham.ahmed@i-ways.net>
2022-02-06 14:54:16 +0500

    metafields UI

--- src/components/Calculator/MetaFields/index.js
+++ src/components/Calculator/MetaFields/index.js
@@ -1,13 +1,46 @@
 import React from "react";
 import "./styles.css";
+import DollarIcon from "../../../resources/images/icon-dollar.svg";
+import UserIcon from "../../../resources/images/icon-person.svg";
 
 const MetaFields = () => {
   return (
-    <div className="wrapper">
-      <span>
-        SPLI <br />
-        TTER
-      </span>
+    <div className="formWrapper">
+      <div className="fieldWrapper">
+        <label>Bill</label>
+        <div className="textField">
+          <input type="number" />
+          <img src={DollarIcon} alt="dollar icon" />
+        </div>
+      </div>
+      <div className="selectTipWrapper">
+        <label>Select Tip %</label>
+        <div className="selectionBox">
+          {["5", "10", "15"].map((item) => (
+            <span className="selectTip" key={item}>
+              {item}%
+            </span>
+          ))}
+        </div>
+        <div className="selectionBox">
+          {["25", "50", ""].map((item) =>
+            item ? (
+              <span className="selectTip" key={item}>
+                {item}%
+              </span>
+            ) : (
+              <input className="selectTip" type="text" placeholder="Custom" />
+            )
+          )}
+        </div>
+      </div>
+      <div className="fieldWrapper">
+        <label>Number of People</label>
+        <div className="textField">
+          <input type="number" />
+          <img src={UserIcon} alt="dollar icon" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import MetaFields from "./MetaFields";
 import Calculations from "./Calculations";
+import LogoImg from "../../resources/images/logo.svg";
 import "./styles.css";
 
 const Calculator = () => {
   return (
     <div className="wrapper">
-      <span>
-        SPLI <br />
-        TTER
-      </span>
+      <img className="logo" src={LogoImg} alt="logo" />
       <div className="calculatorWrapper">
         <MetaFields />
         <Calculations />
